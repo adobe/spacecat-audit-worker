d34c6cf75427d214aaf7e8fcebc11e4accbe2316
Ekrem Doğan <ekremney@users.noreply.github.com>
2024-06-12 14:25:43 +0200

    feat: rum-api-client v2 with customer specific domain key support (#282)

--- src/conversion/handler.js
+++ src/conversion/handler.js
@@ -9,7 +9,7 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-import RUMAPIClient, { createConversionURL } from '@adobe/spacecat-shared-rum-api-client';
+import RUMAPIClient, { createConversionURL } from '@adobe/spacecat-shared-rum-api-client-v1';
 
 import { AuditBuilder } from '../common/audit-builder.js';
 import { getRUMUrl } from '../support/utils.js';
