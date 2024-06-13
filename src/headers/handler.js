fdfba9bb27bee61661f1e380b2057e4f3065cff5
ekremney <ekdogan@adobe.com>
2024-06-13 17:45:36 +0200

    feat: new example headers audit

---
+++ src/headers/handler.js
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2024 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import { AuditBuilder } from '../common/audit-builder.js';
+import { fetch } from '../support/utils.js';
+
+async function runner(auditUrl) {
+  const response = await fetch(auditUrl);
+
+  const headers = response.headers.plain();
+
+  return {
+    auditResult: headers,
+    fullAuditRef: auditUrl,
+  };
+}
+
+export default new AuditBuilder()
+  .withRunner(runner)
+  .build();
