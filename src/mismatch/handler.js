d98433c42c983f809616535561d778d021033e3b
rachel-kittens <rdong@adobe.com>
2024-06-27 16:27:17 -0700

    New mismatch audit

---
+++ src/mismatch/handler.js
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2024 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import RUMAPIClient from '@adobe/spacecat-shared-rum-api-client-v1';
+import { AuditBuilder } from '../common/audit-builder.js';
+
+async function runner(auditUrl, context) {
+  const { log } = context;
+
+  const rumAPIClient = RUMAPIClient.createFrom(context);
+
+  const params = {
+    url: auditUrl,
+    interval: 7,
+  };
+  const result = await rumAPIClient.getRUMDashboard(params);
+
+  log.info(`RUM result for ${auditUrl} is, ${JSON.stringify(result)}`);
+
+  return {
+    auditResult: result,
+    fullAuditRef: auditUrl,
+  };
+}
+export default new AuditBuilder()
+  .withRunner(runner)
+  .build();
