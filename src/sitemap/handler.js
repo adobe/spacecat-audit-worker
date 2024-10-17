b7fa55abb28b2daf758a27ef59a5e2ac5a0e1cdf
paraschi <paraschi@adobe.com>
2024-10-18 00:41:58 +0300

    feat: change method to GET

--- src/sitemap/handler.js
+++ src/sitemap/handler.js
@@ -197,10 +197,11 @@ async function filterValidUrls(urls, log) {
     try {
       const response = await Promise.race([
         fetch(url, {
-          method: 'HEAD',
+          method: 'GET',
           headers: {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
           },
+          redirect: 'follow',
         }),
         timeoutPromise,
       ]);
