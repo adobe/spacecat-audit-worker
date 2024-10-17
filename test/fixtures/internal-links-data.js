c048a1e0cfe0176c3c314b1a43909dd75ba5b60f
Nitin Jadhav <3931042+nitinja@users.noreply.github.com>
2024-10-17 09:16:28 -0500

    Added tests

---
+++ test/fixtures/internal-links-data.js
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2023 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+export const internalLinksData = [
+  {
+    url: 'https://www.example.com/article/dogs/breeds/choosing-an-irish-setter',
+    views: 100,
+    all_sources: [
+      'https://www.example.com/article/dogs/just-for-fun/dogs-good-for-men-13-manly-masculine-dog-breeds',
+    ],
+    source_count: 1,
+    top_source: 'https://www.example.com/article/dogs/just-for-fun/dogs-good-for-men-13-manly-masculine-dog-breeds',
+  },
+  {
+    url: 'https://www.example.com/article/dogs/breeds/choosing-an-german-dog',
+    views: 5,
+    all_sources: [
+      'https://www.example.com/article/dogs/just-for-fun/dogs-good-for-men-8',
+    ],
+    source_count: 1,
+    top_source: 'https://www.example.com/article/dogs/just-for-fun/dogs-good-for-men-8',
+  },
+  {
+    url: '',
+    views: 100,
+    all_sources: [
+      '',
+    ],
+    source_count: 1,
+    top_source: '',
+  },
+  {
+    url: 'https://www.example.com/dogs/the-stages-of-canine-reproduction',
+    views: 100,
+    all_sources: [
+      'android-app://com.google.android.googlequicksearchbox/',
+    ],
+    source_count: 1,
+    top_source: 'android-app://com.google.android.googlequicksearchbox/',
+  },
+  {
+    url: 'https://www.example.com/article/reptiles/general/unusual-pets-praying-mantis',
+    views: 100,
+    all_sources: [
+      'https://www.google.com/',
+    ],
+    source_count: 1,
+    top_source: 'https://www.google.com/',
+  },
+  {
+    url: 'https://www.example.com/article/dogs/breeds/choosing-a-miniature-poodle',
+    views: 100,
+    all_sources: [
+      'https://www.example.com/article/dogs/pet-care/when-is-a-dog-considered-senior',
+    ],
+    source_count: 1,
+    top_source: 'https://www.example.com/article/dogs/pet-care/when-is-a-dog-considered-senior',
+  },
+];
