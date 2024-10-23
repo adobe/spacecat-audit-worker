d8e5a21f64a467a46478e88eaa71cf44865714a6
Nitin Jadhav <3931042+nitinja@users.noreply.github.com>
2024-10-23 09:30:05 -0500

    feat: introduce broken internal links audit (SITES-25870) (#426)
    * initial commit for placeholder files
    
    * disabled lint for now
    
    * disabled covergae for now
    
    * empty commit to trigger build
    
    * disabled coverage for placeholder files
    
    * disabled lint
    
    * fix: trigger release
    
    * empty commit to trigger build
    
    * empty commit to trigger build
    
    * testing out yet another build
    
    * empty commit to trigger build
    
    * Added run client and call
    
    * empty commit to trigger build
    
    * Added working audit code for provided domain
    
    * Error handling and  comments
    
    * Added tests
    
    * Reverted some changes that was made for local testing
    
    * Enhanced test data for code coverage in case of invalid url
    
    * Provided some missing args to handler
    
    * Fixed review comments
    
    * Fixed test data to increse coverage
    
    ---------
    
    Co-authored-by: ekremney <ekdogan@adobe.com>

---
+++ src/internal-links/handler.js
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2024 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import RUMAPIClient from '@adobe/spacecat-shared-rum-api-client';
+import { getRUMDomainkey, getRUMUrl } from '../support/utils.js';
+import { AuditBuilder } from '../common/audit-builder.js';
+import { noopUrlResolver } from '../common/audit.js';
+
+const INTERVAL = 30; // days
+const DAILY_PAGEVIEW_THRESHOLD = 100;
+
+/**
+ * Determines if the URL has the same host as the current host.
+ * @param {*} url
+ * @param {*} currentHost
+ * @returns
+ */
+function hasSameHost(url, currentHost) {
+  const host = new URL(url).hostname;
+  return host === currentHost;
+}
+
+/**
+ * Filter out the 404 links that:
+ * - have less than 100 views and do not have a URL.
+ * - do not have any sources from the same domain.
+ * @param {*} links - all 404 links Data
+ * @param {*} hostUrl - the host URL of the domain
+ * @param {*} auditUrl - the URL to run audit against
+ * @param {*} log - the logger object
+ * @returns {Array} - Returns an array of 404 links that meet the criteria.
+ */
+
+function transform404LinksData(responseData, hostUrl, auditUrl, log) {
+  return responseData.reduce((result, { url, views, all_sources: allSources }) => {
+    try {
+      if (!url || views < DAILY_PAGEVIEW_THRESHOLD) {
+        return result;
+      }
+      const sameDomainSources = allSources.filter(
+        (source) => source && hasSameHost(source, hostUrl),
+      );
+      if (sameDomainSources.length) {
+        result.push({
+          url,
+          views,
+          sources: sameDomainSources,
+        });
+      }
+    } catch {
+      log.error(
+        `Error occurred for audit type broken-internal-links for url ${auditUrl}, while processing sources for link ${url}`,
+      );
+    }
+    return result;
+  }, []);
+}
+/**
+ * Perform an audit to check which internal links for domain are broken.
+ *
+ * @async
+ * @param {string} baseURL - The URL to run audit against
+ * @param {Object} context - The context object containing configurations, services,
+ * and environment variables.
+ * @returns {Response} - Returns a response object indicating the result of the audit process.
+ */
+export async function internalLinksAuditRunner(auditUrl, context, site) {
+  const { log } = context;
+  const finalUrl = await getRUMUrl(auditUrl);
+
+  const rumAPIClient = RUMAPIClient.createFrom(context);
+  const domainkey = await getRUMDomainkey(site.getBaseURL(), context, auditUrl, log);
+
+  const options = {
+    domain: finalUrl,
+    domainkey,
+    interval: INTERVAL,
+    granularity: 'hourly',
+  };
+
+  const all404Links = await rumAPIClient.query('404', options);
+  const auditResult = {
+    internalLinks: transform404LinksData(all404Links, finalUrl, auditUrl, log),
+    auditContext: {
+      interval: INTERVAL,
+    },
+  };
+
+  return {
+    auditResult,
+    fullAuditRef: auditUrl,
+  };
+}
+
+export default new AuditBuilder()
+  .withUrlResolver(noopUrlResolver)
+  .withRunner(internalLinksAuditRunner)
+  .build();
---
+++ test/audits/internal-links.test.js
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2024 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+/* eslint-env mocha */
+
+import { expect, use } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import nock from 'nock';
+import { createSite } from '@adobe/spacecat-shared-data-access/src/models/site.js';
+import { internalLinksAuditRunner } from '../../src/internal-links/handler.js';
+import { internalLinksData } from '../fixtures/internal-links-data.js';
+import { MockContextBuilder } from '../shared.js';
+
+const AUDIT_RESULT_DATA = [
+  {
+    url: 'https://www.example.com/article/dogs/breeds/choosing-an-irish-setter',
+    views: 100,
+    sources: [
+      'https://www.example.com/article/dogs/just-for-fun/dogs-good-for-men-13-manly-masculine-dog-breeds',
+    ],
+  },
+  {
+    url: 'https://www.example.com/article/dogs/breeds/choosing-a-miniature-poodle',
+    views: 100,
+    sources: [
+      'https://www.example.com/article/dogs/pet-care/when-is-a-dog-considered-senior',
+    ],
+  },
+];
+
+use(sinonChai);
+
+const sandbox = sinon.createSandbox();
+
+const baseURL = 'https://example.com';
+const auditUrl = 'www.example.com';
+
+describe('Broken internal links audit', () => {
+  const site = createSite({ baseURL });
+
+  const context = new MockContextBuilder()
+    .withSandbox(sandbox)
+    .withOverrides({
+      runtime: { name: 'aws-lambda', region: 'us-east-1' },
+      func: { package: 'spacecat-services', version: 'ci', name: 'test' },
+      rumApiClient: {
+        query: sinon.stub().resolves(internalLinksData),
+      },
+    })
+    .build();
+
+  beforeEach('setup', () => {
+    nock('https://secretsmanager.us-east-1.amazonaws.com/')
+      .post('/', (body) => body.SecretId === '/helix-deploy/spacecat-services/customer-secrets/example_com/ci')
+      .reply(200, {
+        SecretString: JSON.stringify({
+          RUM_DOMAIN_KEY: 'test-key',
+        }),
+      });
+  });
+
+  afterEach(() => {
+    nock.cleanAll();
+    sinon.restore();
+  });
+
+  it('broken-internal-links audit runs rum api client 404 query', async () => {
+    const result = await internalLinksAuditRunner(
+      'www.example.com',
+      context,
+      site,
+    );
+    expect(context.rumApiClient.query).calledWith('404', {
+      domain: 'www.example.com',
+      domainkey: 'test-key',
+      interval: 30,
+      granularity: 'hourly',
+    });
+    expect(result).to.deep.equal({
+      auditResult: {
+        internalLinks: AUDIT_RESULT_DATA,
+        auditContext: {
+          interval: 30,
+        },
+      },
+      fullAuditRef: auditUrl,
+    });
+  });
+});
