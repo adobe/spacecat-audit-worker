e42c99a8aa43bf07e75bae1da226d67bd663ca41
Damian Zehnder <zehnder@adobe.com>
2024-11-21 10:46:54 +0100

    feat: introduce Opportunity creation for Broken Backlinks

--- src/backlinks/handler.js
+++ src/backlinks/handler.js
@@ -139,13 +139,35 @@ export default async function auditBrokenBacklinks(message, context) {
       auditResult,
     };
 
-    await dataAccess.addAudit(auditData);
+    const audit = await dataAccess.addAudit(auditData);
     const data = {
       type,
       url: site.getBaseURL(),
       auditContext,
       auditResult,
     };
+
+    const opportunityData = {
+      siteId: site.getId(),
+      auditId: audit.getId(),
+      runbook: 'https://adobe.sharepoint.com/:w:/r/sites/aemsites-engineering/_layouts/15/doc2.aspx?sourcedoc=%7BAC174971-BA97-44A9-9560-90BE6C7CF789%7D&file=Experience_Success_Studio_Broken_Backlinks_Runbook.docx&action=default&mobileredirect=true',
+      type: 'broken-backlinks',
+      origin: 'AUTOMATION',
+      title: 'Authoritative Domains are linking to invalid URLs. This could impact your SEO.',
+      description: 'Provide the correct target URL that each of the broken backlinks should be redirected to.',
+      guidance: {
+        steps: [
+          'Review the list of broken target URLs and the suggested redirects.',
+          'Manually override redirect URLs as needed.',
+          'Copy redirects.',
+          'Paste new entries in your website redirects file.',
+          'Publish the changes.',
+        ],
+      },
+      tags: ['traffic-acquisition'],
+    };
+
+    dataAccess.Opportunity.create(opportunityData);
     await sqs.sendMessage(queueUrl, data);
 
     log.info(`Successfully audited ${siteId} for ${type} type audit`);
