8ee6671db36da0a9ac1ac34573e62346bfb7150d
ekremney <ekdogan@adobe.com>
2024-12-13 18:33:19 +0100

    feat(site-detection): final impl and tests

--- src/site-detection/handler.js
+++ src/site-detection/handler.js
@@ -9,15 +9,63 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-/* c8 ignore start */
+
 import { fetch, hasText, stripWWW } from '@adobe/spacecat-shared-utils';
+import URI from 'urijs';
 import { noopPersister, noopUrlResolver } from '../common/audit.js';
 import { AuditBuilder } from '../common/audit-builder.js';
 
 const CORALOGIX_API_URL = 'https://ng-api-http.coralogix.com/api/v1/dataprime/query';
-const CORALOGIX_QUERY = 'source logs | create $d.split_hosts from arraySplit($d.request_x_forwarded_host, \',\') | create $d.xFwHost from $d.split_hosts[0] | distinct $d.xFwHost, $l.subsystemname';
+const CORALOGIX_QUERY = 'source logs | create $d.split_hosts from arraySplit($d.request_x_forwarded_host, \',\') | create $d.domain from $d.split_hosts[0] | distinct $d.domain, $l.subsystemname, $d.request_x_forwarded_host';
 
-async function fetchXFWHosts(authorization, log) {
+const DEFAULT_IGNORED_SUBDOMAIN_TOKENS = ['demo', 'dev', 'stag', 'stg', 'qa', '--', 'sitemap', 'test', 'preview', 'cm-verify', 'owa', 'mail', 'ssl', 'secure', 'publish', 'prod', 'proxy', 'muat', 'edge', 'eds', 'aem'];
+const DEFAULT_IGNORED_DOMAINS = [/helix3.dev/, /fastly.net/, /ngrok-free.app/, /oastify.co/, /fastly-aem.page/, /findmy.media/, /impactful-[0-9]+\.site/, /shuyi-guan/, /adobevipthankyou/, /alshayauat/, /caseytokarchuk/, /\.pfizer$/, /adobeaemcloud.com/, /sabya.xyz/, /magento.com/, /appsechcl.com/, /workers.dev/, /livereview.site/, /localhost/, /lean.delivery/, /kestrelone/];
+
+const IP_ADDRESS_REGEX = /^\d{1,3}(\.\d{1,3}){3}$|^([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$/;
+
+function isValidCandidate(config, domain, log) {
+  /* c8 ignore next 1 */
+  const uri = new URI(domain.startsWith('http') ? domain : `https://${domain}`);
+  const {
+    ignoredSubdomains,
+    ignoredDomains,
+  } = config;
+
+  // x-fw-host header should contain hostname only. If it contains path and/or search
+  // params, then it's most likely a h4ck attempt
+  if (uri.path() !== '/' || uri.query() !== '') {
+    log.info(`Rejected ${domain} because it contains path and/or search params`);
+    return false;
+  }
+
+  // disregard the IP addresses
+  if (IP_ADDRESS_REGEX.test(domain)) {
+    log.info(`Rejected ${domain} because it's an IP address`);
+    return false;
+  }
+
+  // disregard the non-prod hostnames
+  if (ignoredSubdomains.some((ignored) => uri.subdomain().includes(ignored))) {
+    log.info(`Rejected ${domain} because it contains an ignored subdomain`);
+    return false;
+  }
+
+  // disregard unwanted domains
+  if (ignoredDomains.some((ignored) => uri.domain().match(ignored))) {
+    log.info(`Rejected ${domain} because it contains an ignored domain`);
+    return false;
+  }
+
+  // disregard candidates with ports
+  if (hasText(uri.port())) {
+    log.info(`Rejected ${domain} because it contains a port`);
+    return false;
+  }
+
+  return true;
+}
+
+async function fetchCandidates(authorization, log) {
   const endDate = new Date();
   const startDate = new Date(endDate.getTime() - 60 * 60 * 1000);
 
@@ -42,8 +90,11 @@ async function fetchXFWHosts(authorization, log) {
   }
 
   const data = await response.text(); // Since response is NDJSON, treat it as text initially
-  const batches = data.split('\n')
-    .filter(Boolean); // Split the response into batches
+  const batches = data.split('\n').filter(Boolean); // Split the response into batches
+
+  // response may contain duplicate domains since uniqueness is based on
+  // domain - request_x_forwarded_host pair
+  const domains = new Set();
 
   return batches.reduce((acc, batch) => {
     try {
@@ -51,24 +102,27 @@ async function fetchXFWHosts(authorization, log) {
       if (!jsonData.result?.results) return acc;
 
       jsonData.result.results.forEach(({ userData }) => {
+        /* c8 ignore next 1 */
         if (!userData) return;
 
-        try {
-          const { xFwHost, subsystemname } = JSON.parse(userData);
-          if (hasText(xFwHost) && hasText(subsystemname)) {
-            acc.push({
-              xFwHost: stripWWW(xFwHost),
-              hlxVersion: subsystemname,
-            });
-          }
-        } catch (error) {
-          log.error('Error parsing userData:', error);
+        const {
+          request_x_forwarded_host: xFwHost,
+          subsystemname,
+          domain,
+        } = JSON.parse(userData);
+        if (!domains.has(domain) && hasText(xFwHost) && hasText(subsystemname)) {
+          domains.add(domain);
+          acc.push({
+            domain,
+            xFwHost: stripWWW(xFwHost),
+            hlxVersion: subsystemname,
+          });
         }
       });
-    } catch (error) {
-      log.error('Error parsing batch coralogix data:', error);
+    } catch (e) {
+      log.error('Failed to parse Coralogix response');
+      throw e;
     }
-
     return acc;
   }, []);
 }
@@ -91,7 +145,7 @@ async function refeed(xFwHost, hlxVersion, webhook) {
   });
 
   if (!response.ok) {
-    throw new Error(`Failed to re-feed site ${xFwHost}: ${response.statusText}`);
+    throw new Error(`Re-feed request failed with ${response.status}`);
   }
 }
 
@@ -104,30 +158,37 @@ export async function siteDetectionRunner(_, context) {
   const {
     CORALOGIX_API_KEY: authorization,
     SITE_DETECTION_WEBHOOK: siteDetectionWebHook,
+    SITE_DETECTION_IGNORED_DOMAINS: ignoredDomains = DEFAULT_IGNORED_DOMAINS,
+    SITE_DETECTION_IGNORED_SUBDOMAIN_TOKENS: ignoredSubdomains = DEFAULT_IGNORED_SUBDOMAIN_TOKENS,
   } = env;
 
+  const config = {
+    ignoredDomains,
+    ignoredSubdomains,
+  };
+
   const sites = await dataAccess.getSites();
-  log.info(`Sites: ${sites.length}`);
   const siteCandidates = await dataAccess.getSiteCandidates();
-  log.info(`Site candidates: ${siteCandidates.length}`);
 
   const knownHosts = new Set([...sites, ...siteCandidates]
     .map((s) => s.getBaseURL())
     .map((url) => url.replace(/^https?:\/\//, '')));
 
-  log.info(`Known hosts: ${knownHosts.size}`);
+  let candidates = await fetchCandidates(authorization, log);
 
-  const xFwHosts = await fetchXFWHosts(authorization, log);
-  log.info(`xFwHosts: ${JSON.stringify(xFwHosts)}`);
-  const unknownHosts = xFwHosts.filter((host) => !knownHosts.has(host.xFwHost));
-  log.info(`Unknown hosts: ${JSON.stringify(unknownHosts)}`);
+  candidates = candidates
+    .filter((candidate) => !knownHosts.has(candidate.domain))
+    .filter((candidate) => isValidCandidate(config, candidate.domain, log));
 
-  for (const unknownHost of unknownHosts) {
+  log.info(`Found ${candidates.length} valid candidates`);
+
+  // TODO: replace the HOOK call with a proper post-processing step
+  for (const candidate of candidates) {
     try {
       // eslint-disable-next-line no-await-in-loop
-      await refeed(unknownHost.xFwHost, unknownHost.hlxVersion, siteDetectionWebHook);
+      await refeed(candidate.xFwHost, candidate.hlxVersion, siteDetectionWebHook);
     } catch (e) {
-      log.warn(`Failed to re-feed ${unknownHost}: ${e.message}`);
+      log.warn(`Failed to re-feed ${candidate.domain}: ${e.message}`);
     }
   }
 }
@@ -138,4 +199,3 @@ export default new AuditBuilder()
   .withMessageSender(() => ({}))
   .withRunner(siteDetectionRunner)
   .build();
-/* c8 ignore end */
