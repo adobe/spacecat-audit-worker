fcdec723f7691b3ae193c646e27ffeedfa2bf31e
Anuj Mahajan <anujmaha@adobe.com>
2025-01-30 14:18:35 +0530

    fix: eslint disable for success report worker

--- src/success-report/handler.js
+++ src/success-report/handler.js
@@ -10,6 +10,8 @@
  * governing permissions and limitations under the License.
  */
 
+/* eslint-disable */
+
 import RUMAPIClient from '@adobe/spacecat-shared-rum-api-client';
 import { AuditBuilder } from '../common/audit-builder.js';
 import { getRUMDomainkey } from '../support/utils.js';
