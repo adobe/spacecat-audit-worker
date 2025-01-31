8e08a696372f23a3f7d3fb0d01c4d5802b8197f9
Divyansh Pratap <dipratap@adobe.com>
2025-01-31 01:07:30 +0100

    fix: temp change

--- src/metatags/seo-checks.js
+++ src/metatags/seo-checks.js
@@ -207,7 +207,7 @@ class SeoChecks {
   getDetectedTags() {
     // return this.detectedTags;
     // temporary change for demo, will be reverted
-    return Object.fromEntries(Object.entries(this.detectedTags).slice(40, 85));
+    return Object.fromEntries(Object.entries(this.detectedTags).slice(30, 66));
   }
 
   /**
