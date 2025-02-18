bb060df011f07d933d24c61b6418ff234a5d4f02
Josep Lopez <joselopez@adobe.com>
2025-02-18 11:23:36 +0100

    debug: get more information about incorrect payload for Firefall

--- src/image-alt-text/suggestion-helper.js
+++ src/image-alt-text/suggestion-helper.js
@@ -26,7 +26,13 @@ export default async function generateSuggestions(auditUrl, auditData, context) 
     responseFormat: 'json_object',
   };
   const prompt = 'Using these alt text best practices, describe the picture provided in a way that is helpful for the user. Alt Text Best Practices: Keep it short, usually 1-2 sentences. Dont overthink it. Consider key elements of why you chose this image, instead of describing every little detail. No need to say image of or picture of. But, do say if its a logo, illustration, painting, or cartoon. Dont duplicate text thats adjacent in the document or website. End the alt text sentence with a period.';
-  const response = await firefallClient.fetchChatCompletion(prompt, firefallOptions);
 
-  log.debug('Firefall response for alt-text suggestions', response);
+  log.debug('About to call Firefall for alt-text suggestion generation');
+
+  try {
+    const response = await firefallClient.fetchChatCompletion(prompt, firefallOptions);
+    log.debug('Firefall response for alt-text suggestions', response);
+  } catch (err) {
+    log.error('Error calling Firefall for alt-text suggestion generation', err);
+  }
 }
