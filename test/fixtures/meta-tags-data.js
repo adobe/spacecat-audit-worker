0cfb00926c64af650cc20a19d2e2c3fd2565345e
Sahil Silare <150108834+ssilare-adobe@users.noreply.github.com>
2025-02-21 12:26:13 +0530

    fix: sites-28998 fix for ignored entries are overwritten with next audit run (#653)
    * fix: sites-28998 fix for ignored entries are overwritten with next audit run
    
    Signed-off-by: Sahil Silare <sahilsilareadobe@gmail.com>
    
    * Revert "fix: sites-28998 fix for ignored entries are overwritten with next audit run"
    
    This reverts commit 0db8edec4543915deab7dec9726b9abe60ca1132.
    
    * fix: testing the changes
    
    * fix: removed syncMetatagsSuggestions function
    
    * fix: removed unused test
    
    * chore: renamed file to `opportunity-utils.js`
    
    ---------
    
    Signed-off-by: Sahil Silare <sahilsilareadobe@gmail.com>

--- test/fixtures/meta-tags-data.js
+++ test/fixtures/meta-tags-data.js
@@ -197,6 +197,8 @@ const testData = {
       },
       remove: () => {},
       getStatus: () => 'NEW',
+      setData: () => {},
+      save: () => {},
       getData: () => ({
         tagContent: 'The BigOne Insert modified',
         seoRecommendation: '140-160 characters long',
@@ -226,6 +228,8 @@ const testData = {
       },
       remove: () => {},
       getStatus: () => 'SKIPPED',
+      setData: () => {},
+      save: () => {},
       getData: () => ({
         seoRecommendation: 'Should be present',
         issue: 'Missing H1',
@@ -255,6 +259,8 @@ const testData = {
       },
       remove: () => {},
       getStatus: () => 'NEW',
+      setData: () => {},
+      save: () => {},
       getData: () => ({
         tagContent: '["We Can All Win Together","We Say As We Do"]',
         seoRecommendation: '1 H1 on a page',
