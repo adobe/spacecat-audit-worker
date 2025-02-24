c42759be9e3276b224317d05561b8ed72e9ba819
Rahul Kumar Singh <rahul@Rahuls-MacBook-Pro-5.local>
2025-02-25 00:50:33 +0530

    feat: audit step in forms

--- src/forms-opportunities/opportunityHandler.js
+++ src/forms-opportunities/opportunityHandler.js
@@ -20,27 +20,30 @@ import generateOpptyData from './utils.js';
 export default async function convertToOpportunity(auditUrl, auditData, context) {
   const { dataAccess, log } = context;
   const { Opportunity } = dataAccess;
+  // eslint-disable-next-line no-param-reassign
+  auditData = JSON.parse(JSON.stringify(auditData));
+
   log.info(`Debug log 93 latestAudit ${JSON.stringify(auditData, null, 2)}`);
   log.info('auditData type:', typeof auditData);
   log.info('auditData keys:', Object.keys(auditData));
-  log.info('Direct siteId value:', auditData.getSiteId());
+  log.info('Direct siteId value:', auditData.siteId);
 
   const { siteId } = auditData;
   log.info('site id :', typeof siteId);
 
-  log.info(`Syncing opportunity for ${auditData.getSiteId()}`);
+  log.info(`Syncing opportunity for ${auditData.siteId}`);
   let highFormViewsLowConversionsOppty;
 
   try {
-    const opportunities = await Opportunity.allBySiteIdAndStatus(auditData.getSiteId(), 'NEW');
+    const opportunities = await Opportunity.allBySiteIdAndStatus(auditData.siteId, 'NEW');
 
     highFormViewsLowConversionsOppty = opportunities.find((oppty) => oppty.getType() === 'high-form-views-low-conversions');
   } catch (e) {
-    log.error(`Fetching opportunities for siteId ${auditData.getSiteId()} failed with error: ${e.message}`);
-    throw new Error(`Failed to fetch opportunities for siteId ${auditData.getSiteId()}: ${e.message}`);
+    log.error(`Fetching opportunities for siteId ${auditData.siteId} failed with error: ${e.message}`);
+    throw new Error(`Failed to fetch opportunities for siteId ${auditData.siteId}: ${e.message}`);
   }
 
-  const { formVitals } = auditData.getAuditResult();
+  const { formVitals } = auditData.auditResult;
 
   const formOpportunities = generateOpptyData(formVitals);
 
@@ -48,10 +51,9 @@ export default async function convertToOpportunity(auditUrl, auditData, context)
     for (const opptyData of formOpportunities) {
       if (!highFormViewsLowConversionsOppty) {
         const opportunityData = {
-          siteId: auditData.getSiteId(),
+          siteId: auditData.siteId,
           // auditId: auditData.id,
-          // auditId: auditData.getLatestAuditId(),
-          auditId: auditData.getAuditId(),
+          auditId: auditData.latestAuditId,
           runbook: 'https://adobe.sharepoint.com/:w:/s/AEM_Forms/EU_cqrV92jNIlz8q9gxGaOMBSRbcwT9FPpQX84bRKQ9Phw?e=Nw9ZRz',
           type: 'high-form-views-low-conversions',
           origin: 'AUTOMATION',
@@ -66,14 +68,14 @@ export default async function convertToOpportunity(auditUrl, auditData, context)
         highFormViewsLowConversionsOppty = await Opportunity.create(opportunityData);
         log.debug('Forms Opportunity created');
       } else {
-        highFormViewsLowConversionsOppty.setAuditId(auditData.getSiteId());
+        highFormViewsLowConversionsOppty.setAuditId(auditData.siteId);
         // eslint-disable-next-line no-await-in-loop
         await highFormViewsLowConversionsOppty.save();
       }
     }
   } catch (e) {
-    log.error(`Creating Forms opportunity for siteId ${auditData.getSiteId()} failed with error: ${e.message}`, e);
-    throw new Error(`Failed to create Forms opportunity for siteId ${auditData.getSiteId()}: ${e.message}`);
+    log.error(`Creating Forms opportunity for siteId ${auditData.siteId} failed with error: ${e.message}`, e);
+    throw new Error(`Failed to create Forms opportunity for siteId ${auditData.siteId}: ${e.message}`);
   }
-  log.info(`Successfully synced Opportunity for site: ${auditData.getSiteId()} and high-form-views-low-conversions audit type.`);
+  log.info(`Successfully synced Opportunity for site: ${auditData.siteId} and high-form-views-low-conversions audit type.`);
 }
