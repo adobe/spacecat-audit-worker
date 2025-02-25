b4bfb26bad1033fd7263b51dbc234816e4d65208
Igor Grubic <igrubic@adobe.com>
2025-02-25 14:00:04 +0100

    ASSETS-47371: prepare for pr

--- src/image-alt-text/suggestionsEngine.js
+++ src/image-alt-text/suggestionsEngine.js
@@ -17,12 +17,10 @@ const PROMPT_FILE = 'image-alt-text';
 const BATCH_SIZE = 10;
 const BATCH_DELAY = 5000;
 const MODEL = 'gpt-4o';
-const SUPPORTED_FORMATS = /\.(webp|png|gif|jpeg)(?=\?|$)/i;
+// https://platform.openai.com/docs/guides/vision
+const SUPPORTED_FORMATS = /\.(webp|png|gif|jpeg|jpg)(?=\?|$)/i;
 
-const getImageSuggestions = async (imageUrls, auditUrl, context) => {
-  const { log } = context;
-  const firefallClient = FirefallClient.createFrom(context);
-
+const filterImages = (imageUrls, auditUrl) => {
   const imagesFromHost = [];
   const otherImages = [];
   const unsupportedFormatImages = [];
@@ -37,61 +35,74 @@ const getImageSuggestions = async (imageUrls, auditUrl, context) => {
     }
   });
 
-  log.info('[alt-text] Total images from host:', imagesFromHost.length);
-  log.info('[alt-text] Other images:', otherImages);
-  log.info('[alt-text] Unsupported format images:', unsupportedFormatImages);
+  return { imagesFromHost, otherImages, unsupportedFormatImages };
+};
 
-  const imageList = imagesFromHost;
-
-  function chunkArray(array, chunkSize) {
-    const chunks = [];
-    for (let i = 0; i < array.length; i += chunkSize) {
-      chunks.push(array.slice(i, i + chunkSize));
-    }
-    return chunks;
+const chunkArray = (array, chunkSize) => {
+  const chunks = [];
+  for (let i = 0; i < array.length; i += chunkSize) {
+    chunks.push(array.slice(i, i + chunkSize));
   }
+  return chunks;
+};
 
-  const imageBatches = chunkArray(imageList, BATCH_SIZE);
+const generateBatchPromises = (
+  imageBatches,
+  firefallClient,
+  log,
+) => imageBatches.map(async (batch, index) => {
+  await sleep(index * BATCH_DELAY);
 
-  const batchPromises = imageBatches.map(async (batch, index) => {
-    await sleep(index * BATCH_DELAY);
-
-    const firefallOptions = {
-      imageUrls: batch,
-      model: MODEL,
-      additionalHeaders: {
-        'Cache-Control': 'no-cache',
-        Pragma: 'no-cache',
-      },
-    };
-    const prompt = await getPrompt({ images: batch }, PROMPT_FILE, log);
-    try {
-      const response = await firefallClient.fetchChatCompletion(prompt, firefallOptions);
-      if (response.choices?.length >= 1 && response.choices[0].finish_reason !== 'stop') {
-        log.error('[alt-text] No final suggestions found for batch');
-      }
-
-      const answer = JSON.parse(response.choices[0].message.content);
-      log.info(`[alt-text] Loaded ${answer.length} alt-text suggestions for batch`);
-      return answer;
-    } catch (err) {
-      log.error('[alt-text] Error calling Firefall for alt-text suggestion generation for batch', err);
-      return [];
+  const firefallOptions = {
+    imageUrls: batch,
+    model: MODEL,
+    additionalHeaders: {
+      'Cache-Control': 'no-cache',
+      Pragma: 'no-cache',
+    },
+  };
+  const prompt = await getPrompt({ images: batch }, PROMPT_FILE, log);
+  try {
+    const response = await firefallClient.fetchChatCompletion(prompt, firefallOptions);
+    if (response.choices?.length >= 1 && response.choices[0].finish_reason !== 'stop') {
+      log.error('[alt-text] No final suggestions found for batch');
     }
-  });
 
-  const results = await Promise.all(batchPromises);
+    const answer = JSON.parse(response.choices[0].message.content);
+    log.info(`[alt-text] Loaded ${answer.length} alt-text suggestions for batch`);
+    return answer;
+  } catch (err) {
+    log.error('[alt-text] Error calling Firefall for alt-text suggestion generation for batch', err);
+    return [];
+  }
+});
 
-  const finalResults = results.reduce((acc, result) => {
+const getImageSuggestions = async (imageUrls, auditUrl, context) => {
+  const { log } = context;
+  const firefallClient = FirefallClient.createFrom(context);
+
+  const filteredImages = filterImages(imageUrls, auditUrl);
+
+  log.info('[alt-text] Total images from host:', filteredImages.imagesFromHost.length);
+  log.info('[alt-text] Other images:', filteredImages.otherImages);
+  log.info('[alt-text] Unsupported format images:', filteredImages.unsupportedFormatImages);
+
+  const supportedImageBatches = chunkArray(filteredImages.imagesFromHost, BATCH_SIZE);
+
+  const batchPromises = generateBatchPromises(supportedImageBatches, firefallClient, log);
+
+  const batchedResults = await Promise.all(batchPromises);
+
+  const suggestionsByImageUrl = batchedResults.reduce((acc, result) => {
     result.forEach((item) => {
       acc[item.image_url] = item;
     });
     return acc;
   }, {});
 
-  log.info(`[alt-text] Final Merged Suggestions: ${Object.keys(finalResults).length}`);
+  log.info(`[alt-text] Final Merged Suggestions: ${Object.keys(suggestionsByImageUrl).length}`);
 
-  return finalResults;
+  return suggestionsByImageUrl;
 };
 
 export default {
