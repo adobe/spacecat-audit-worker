f81ef3d7650d21b127713b664b1b9ab95558bea5
Iulia Grumaz <grumaz@adobe.com>
2025-02-27 17:28:22 +0200

    fix(override-base-url): use overrideBaseURL from site.config.fetchConfig

--- src/common/runner-audit.js
+++ src/common/runner-audit.js
@@ -41,6 +41,7 @@ export class RunnerAudit extends BaseAudit {
       }
 
       const finalUrl = await this.urlResolver(site);
+      log.debug(`DEBUUUUUUG: Running ${type} audit for site ${siteId} at final URL ${finalUrl}`);
       const result = await this.runner(finalUrl, context, site);
 
       return this.processAuditResult(
