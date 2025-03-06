7111f5937ae9a9188265933798b6c4048ab43b6d
Damian Zehnder <16799758+dzehnder@users.noreply.github.com>
2025-03-06 09:00:04 +0100

    feat: introduce KPI metrics for broken-backlinks (#725)

--- src/backlinks/opportunity-data-mapper.js
+++ src/backlinks/opportunity-data-mapper.js
@@ -10,7 +10,7 @@
  * governing permissions and limitations under the License.
  */
 
-export function createOpportunityData() {
+export function createOpportunityData(kpiMetrics) {
   return {
     runbook: 'https://adobe.sharepoint.com/:w:/r/sites/aemsites-engineering/_layouts/15/doc2.aspx?sourcedoc=%7BAC174971-BA97-44A9-9560-90BE6C7CF789%7D&file=Experience_Success_Studio_Broken_Backlinks_Runbook.docx&action=default&mobileredirect=true',
     origin: 'AUTOMATION',
@@ -26,6 +26,8 @@ export function createOpportunityData() {
       ],
     },
     tags: ['Traffic acquisition'],
-    data: null,
+    data: {
+      ...kpiMetrics,
+    },
   };
 }
