73f7a55194e2ea856562892cce866c8173372ad4
Nitin Gupta <niting.iitr@gmail.com>
2025-03-06 23:33:13 +0530

    fix: update title for forms opporty (#751)
    Please ensure your pull request adheres to the following guidelines:
    - [ ] make sure to link the related issues in this description
    - [ ] when merging / squashing, make sure the fixed issue references are
    visible in the commits, for easy compilation of release notes
    
    ## Related Issues
    
    
    Thanks for contributing!

--- src/forms-opportunities/opportunityHandler.js
+++ src/forms-opportunities/opportunityHandler.js
@@ -54,7 +54,7 @@ export default async function convertToOpportunity(auditUrl, auditDataObject, sc
         runbook: 'https://adobe.sharepoint.com/:w:/s/AEM_Forms/EU_cqrV92jNIlz8q9gxGaOMBSRbcwT9FPpQX84bRKQ9Phw?e=Nw9ZRz',
         type: 'high-form-views-low-conversions',
         origin: 'AUTOMATION',
-        title: 'Form has high views but low conversions',
+        title: 'Form has low conversions',
         description: 'Form has high views but low conversions',
         tags: ['Forms Conversion'],
         data: {
