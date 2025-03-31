eb699ab8c7b4fad46137592f4c88a5f2cc97e606
igor-grubic <igor.grubic@gmail.com>
2025-03-31 15:21:16 +0200

    fix-alt-text-prompt-experiment: changing logs

--- src/image-alt-text/suggestionsEngine.js
+++ src/image-alt-text/suggestionsEngine.js
@@ -32,7 +32,6 @@ const chunkArray = (array, chunkSize) => {
 
 const getFirefallResponse = async (prompt, firefallClient, firefallOptions, log) => {
   try {
-    log.info(`[${AUDIT_TYPE}]: Prompt: ${prompt}`);
     const response = await firefallClient.fetchChatCompletion(prompt, firefallOptions);
     if (isNonEmptyArray(response.choices) && response.choices[0].finish_reason !== 'stop') {
       log.error(`[${AUDIT_TYPE}]: No final suggestions found for batch`);
@@ -43,6 +42,7 @@ const getFirefallResponse = async (prompt, firefallClient, firefallOptions, log)
     return answer;
   } catch (err) {
     log.error(`[${AUDIT_TYPE}]: Error calling Firefall for alt-text suggestion generation for batch: ${prompt}`, err);
+    log.error(`[${AUDIT_TYPE}]: With images: ${firefallOptions.imageUrls.join(', ')}`);
     return [];
   }
 };
