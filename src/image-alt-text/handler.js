9a7d9ff7fc8b8c0c1a5c962bbaae75c9316411f1
Josep Lopez <me@jay.cat>
2025-05-05 11:09:45 +0200

    fix: change URL resolver

--- src/image-alt-text/handler.js
+++ src/image-alt-text/handler.js
@@ -172,7 +172,7 @@ export async function processAltTextAuditStep(context) {
 }
 
 export default new AuditBuilder()
-  .withUrlResolver((site) => site.resolveFinalURL())
+  .withUrlResolver((site) => site.getBaseURL())
   .addStep('processImport', processImportStep, AUDIT_STEP_DESTINATIONS.IMPORT_WORKER)
   .addStep('prepareScraping', prepareScrapingStep, AUDIT_STEP_DESTINATIONS.CONTENT_SCRAPER)
   .addStep('processAltTextAudit', processAltTextAuditStep)
