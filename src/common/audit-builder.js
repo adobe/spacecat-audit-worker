dd0ded5d5a758c4f698d7ee79175d73a644c60bd
Damian Zehnder <zehnder@adobe.com>
2025-05-09 14:13:14 +0300

    fix: audit steps

--- src/common/audit-builder.js
+++ src/common/audit-builder.js
@@ -148,7 +148,7 @@ export class AuditBuilder {
           this.persister,
           this.messageSender,
           this.postProcessors,
-          stepNames,
+          this.steps,
         );
       }
 
