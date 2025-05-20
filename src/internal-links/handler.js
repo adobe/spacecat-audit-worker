b5c9bd7c7bb3d860f8afadc050daf315cf3ac930
Nitin Jadhav <3931042+nitinja@users.noreply.github.com>
2025-05-20 03:09:42 -0500

    Fix: fixed RUM url issue for sites like bhhs.com (#877)
    Please ensure your pull request adheres to the following guidelines:
    - [ ] make sure to link the related issues in this description
    - [ ] when merging / squashing, make sure the fixed issue references are
    visible in the commits, for easy compilation of release notes
    
    ## Related Issues
    
    
    Thanks for contributing!

--- src/internal-links/handler.js
+++ src/internal-links/handler.js
@@ -13,13 +13,12 @@
 import RUMAPIClient from '@adobe/spacecat-shared-rum-api-client';
 import { Audit, Opportunity as Oppty, Suggestion as SuggestionDataAccess } from '@adobe/spacecat-shared-data-access';
 import { isNonEmptyArray } from '@adobe/spacecat-shared-utils';
-import { getRUMUrl } from '../support/utils.js';
 import { AuditBuilder } from '../common/audit-builder.js';
-import { noopUrlResolver } from '../common/index.js';
 import { syncSuggestions } from '../utils/data-access.js';
 import { convertToOpportunity } from '../common/opportunity.js';
 import { createOpportunityData } from './opportunity-data-mapper.js';
 import { generateSuggestionData } from './suggestions-generator.js';
+import { wwwUrlResolver } from '../common/index.js';
 import {
   calculateKpiDeltasForAudit,
   isLinkInaccessible,
@@ -41,7 +40,7 @@ const AUDIT_TYPE = Audit.AUDIT_TYPES.BROKEN_INTERNAL_LINKS;
  */
 export async function internalLinksAuditRunner(auditUrl, context) {
   const { log, site } = context;
-  const finalUrl = await getRUMUrl(auditUrl);
+  const finalUrl = await wwwUrlResolver(site, context);
 
   try {
     const rumAPIClient = RUMAPIClient.createFrom(context);
@@ -221,7 +220,7 @@ export async function opportunityAndSuggestionsStep(context) {
 }
 
 export default new AuditBuilder()
-  .withUrlResolver(noopUrlResolver)
+  .withUrlResolver(wwwUrlResolver)
   .addStep(
     'runAuditAndImportTopPages',
     runAuditAndImportTopPagesStep,
