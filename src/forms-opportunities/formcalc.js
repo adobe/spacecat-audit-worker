6931cf47bfc8a6314dda5507c9d04f2a3c6189c7
vdua <vdua@adobe.com>
2025-06-02 05:16:25 +0530

    fixing tests

--- src/forms-opportunities/formcalc.js
+++ src/forms-opportunities/formcalc.js
@@ -28,7 +28,7 @@ function aggregateFormVitalsByDevice(formVitalsCollection) {
   formVitalsCollection.forEach((item) => {
     const {
       url, formview = {}, formengagement = {}, pageview = {}, formsubmit = {},
-      trafficacquisition = {}, formsource = '', iframeSrc, cta,
+      trafficacquisition = {}, formsource = '', iframeSrc, formCTAWithinPage,
     } = item;
 
     const totals = {
@@ -57,7 +57,9 @@ function aggregateFormVitalsByDevice(formVitalsCollection) {
     totals.formsubmit = calculateSums(formsubmit, totals.formsubmit);
     totals.trafficacquisition = trafficacquisition;
     totals.formsource = formsource.startsWith('dialog') ? formsource.replace('dialog ', '') : formsource;
-    totals.cta = cta;
+    if (formCTAWithinPage) {
+      totals.formCTAWithinPage = formCTAWithinPage;
+    }
     if (iframeSrc) {
       totals.iframeSrc = iframeSrc;
     }
@@ -128,6 +130,7 @@ export function getHighFormViewsLowConversionMetrics(formVitalsCollection) {
 export function getHighPageViewsLowFormViewsMetrics(formVitalsCollection) {
   const urls = [];
   const resultMap = aggregateFormVitalsByDevice(formVitalsCollection);
+
   resultMap.forEach((metrics, url) => {
     const { total: pageViews } = metrics.pageview;
     const { total: formViews } = metrics.formview;
@@ -136,7 +139,6 @@ export function getHighPageViewsLowFormViewsMetrics(formVitalsCollection) {
     if (hasHighViews(pageViews) && hasLowFormViews(pageViews, formViews, formEngagement)) {
       urls.push({
         url,
-        cta: metrics.cta,
         ...metrics,
       });
     }
