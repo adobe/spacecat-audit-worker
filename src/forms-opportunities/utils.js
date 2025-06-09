28beef40833b327d14c389a7cce0d1aacf262d89
Rahul Kumar Singh <rahulkumar_2007@yahoo.com>
2025-06-09 19:26:57 +0530

    feat: adding projected conversion value in forms opportunities (#940)
    Please ensure your pull request adheres to the following guidelines:
    - [ ] make sure to link the related issues in this description
    - [ ] when merging / squashing, make sure the fixed issue references are
    visible in the commits, for easy compilation of release notes
    - [ ] If data sources for any opportunity has been updated/added, please
    update the
    [wiki](https://wiki.corp.adobe.com/display/AEMSites/Data+Sources+for+Opportunities)
    for same opportunity.
    
    ## Related Issues
    
    
    Thanks for contributing!
    
    ---------
    
    Co-authored-by: Rahul Kumar Singh <rahul@Rahuls-MacBook-Pro-5.local>

--- src/forms-opportunities/utils.js
+++ src/forms-opportunities/utils.js
@@ -11,6 +11,7 @@
  */
 
 import {
+  FORMS_AUDIT_INTERVAL,
   isNonEmptyArray,
 } from '@adobe/spacecat-shared-utils';
 import { GetObjectCommand } from '@aws-sdk/client-s3';
@@ -20,8 +21,10 @@ import {
   getHighPageViewsLowFormViewsMetrics,
 } from './formcalc.js';
 import { FORM_OPPORTUNITY_TYPES, successCriteriaLinks } from './constants.js';
+import { calculateCPCValue } from '../support/utils.js';
 
 const EXPIRY_IN_SECONDS = 3600 * 24 * 7;
+const CONVERSION_BOOST = 0.2;
 
 function getS3PathPrefix(url, site) {
   const urlObj = new URL(url);
@@ -426,4 +429,55 @@ export function getSuccessCriteriaDetails(criteria) {
     criteriaNumber: successCriteriaNumber,
     understandingUrl: successCriteriaDetails.understandingUrl,
   };
+}
+
+// eslint-disable-next-line no-shadow
+function getCostSaved(originalTraffic, conversionRate, cpc, conversionBoost) {
+  if (conversionRate === 0) {
+    return 0;
+  }
+  const originalConversions = originalTraffic * conversionRate;
+  const newConversionRate = conversionRate * (1 + conversionBoost);
+  const newTrafficNeeded = originalConversions / newConversionRate;
+  const trafficDelta = originalTraffic - newTrafficNeeded;
+  const costSaved = trafficDelta * cpc;
+
+  return parseFloat(costSaved.toFixed(2));
+}
+
+/**
+ * Calculates the projected conversion value for a form based on its views and CPC
+ * @param {Object} context - The context object containing necessary dependencies
+ * @param {string} siteId - The site ID
+ * @param {Object} formMetrics - The form metrics object containing traffic data
+ * @returns {Promise<Object>} Object containing cpcValue and projectedConversionValue
+ */
+export async function calculateProjectedConversionValue(context, siteId, opportunityData) {
+  const { log } = context;
+
+  try {
+    const cpcValue = await calculateCPCValue(context, siteId);
+    log.info(`Calculated CPC value: ${cpcValue} for site: ${siteId}`);
+
+    const originalTraffic = opportunityData.pageViews;
+    const conversionRate = opportunityData?.metrics?.find(
+      (m) => m?.type === 'conversionRate' && m?.device === '*',
+    )?.value?.page ?? 0;
+
+    // traffic is calculated for 15 days - extrapolating for a year
+    const trafficPerYear = Math.floor((originalTraffic / FORMS_AUDIT_INTERVAL)) * 365;
+    const projectedConversionValue = getCostSaved(
+      trafficPerYear,
+      conversionRate,
+      cpcValue,
+      CONVERSION_BOOST,
+    );
+
+    return {
+      projectedConversionValue,
+    };
+  } catch (error) {
+    log.error(`Error calculating projected conversion value for site ${siteId}:`, error);
+    return null;
+  }
 }
