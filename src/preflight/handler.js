d1c810fad775a0514c8a96835a48cdd8cb6c0c8b
anagarwa <anagarwa@adobe.com>
2025-06-10 14:58:50 +0530

    fix: handling imsToken for scrapper

--- src/preflight/handler.js
+++ src/preflight/handler.js
@@ -53,7 +53,7 @@ export async function scrapePages(context) {
   const siteId = site.getId();
 
   const jobMetadata = job.getMetadata();
-  const { urls } = jobMetadata.payload;
+  const { urls, imsTokenRefId } = jobMetadata.payload;
 
   if (!isValidUrls(urls)) {
     throw new Error(`[preflight-audit] site: ${siteId}. Invalid urls provided for scraping`);
@@ -72,6 +72,7 @@ export async function scrapePages(context) {
     options: {
       enableAuthentication: true,
       screenshotTypes: [],
+      imsTokenRefId,
     },
   };
 }
