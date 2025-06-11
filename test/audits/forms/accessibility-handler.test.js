ac7bea8fd9cad6dc5cffea9eb241b0a54092d353
Deepak Maurya <32591299+dmaurya929@users.noreply.github.com>
2025-06-11 19:47:01 +0530

    feat(forms): form a11y handlers communication with M (#933)
    - Added new guidance handlers for form accessibility.
    - Introduced a new handler for processing messages from mystique
    regarding detected accessibility issues.
    - Updated tests to cover new functionality and ensure proper handling of
    accessibility data.

--- test/audits/forms/accessibility-handler.test.js
+++ test/audits/forms/accessibility-handler.test.js
@@ -17,7 +17,7 @@ import sinon from 'sinon';
 import nock from 'nock';
 import sinonChai from 'sinon-chai';
 import { FORM_OPPORTUNITY_TYPES } from '../../../src/forms-opportunities/constants.js';
-import createAccessibilityOpportunity from '../../../src/forms-opportunities/oppty-handlers/accessibility-handler.js';
+import mystiqueDetectedFormAccessibilityHandler, { createAccessibilityOpportunity } from '../../../src/forms-opportunities/oppty-handlers/accessibility-handler.js';
 import { MockContextBuilder } from '../../shared.js';
 
 use(sinonChai);
@@ -46,12 +46,17 @@ describe('Forms Opportunities - Accessibility Handler', () => {
           site: {
             getId: sinon.stub().returns(siteId),
             getBaseURL: sinon.stub().returns('https://example.com'),
+            getDeliveryType: sinon.stub().returns('aem'),
           },
           env: {
             S3_SCRAPER_BUCKET_NAME: bucketName,
+            QUEUE_SPACECAT_TO_MYSTIQUE: 'test-queue',
           },
           s3Client: {
             send: sandbox.stub(),
+          },
+          sqs: {
+            sendMessage: sandbox.stub().resolves(),
           },
           log: {
             info: sinon.stub(),
@@ -60,7 +65,8 @@ describe('Forms Opportunities - Accessibility Handler', () => {
           dataAccess: {
             Opportunity: {
               allBySiteIdAndStatus: sandbox.stub().resolves([]),
-              create: sandbox.stub().resolves(),
+              create: sandbox.stub().resolves({ getId: () => 'test-opportunity-id' }),
+              findById: sandbox.stub().resolves(null),
             },
           },
         })
@@ -151,7 +157,51 @@ describe('Forms Opportunities - Accessibility Handler', () => {
       await createAccessibilityOpportunity(latestAudit, context);
       expect(context.dataAccess.Opportunity.create).to.not.have.been.called;
       expect(context.log.info).to.have.been.calledWith(
-        `[Form Opportunity] [Site Id: ${siteId}] No a11y data found`,
+        `[Form Opportunity] [Site Id: ${siteId}] No a11y data found to create or update opportunity `,
+      );
+    });
+
+    it('should fail when no a11yIssues are present', async () => {
+      const latestAudit = {
+        siteId,
+        auditId: 'test-audit-id',
+        getSiteId: () => siteId,
+        getAuditId: () => 'test-audit-id',
+      };
+
+      context.s3Client.send
+        .onFirstCall()
+        .resolves({
+          CommonPrefixes: [
+            { Prefix: `forms-accessibility/${siteId}/${version}/` },
+          ],
+        })
+        .onSecondCall()
+        .resolves({
+          Contents: [
+            { Key: `forms-accessibility/${siteId}/${version}/form1.json` },
+          ],
+          IsTruncated: false,
+        })
+        .onThirdCall()
+        .resolves({
+          ContentType: 'application/json',
+          Body: {
+            transformToString: sandbox.stub().resolves(JSON.stringify({
+              finalUrl: 'https://example.com/form1',
+              a11yResult: [{
+                form: 'https://example.com/form1',
+                formSource: '#form1',
+                a11yIssues: [],
+              }],
+            })),
+          },
+        });
+
+      await createAccessibilityOpportunity(latestAudit, context);
+      expect(context.dataAccess.Opportunity.create).to.not.have.been.called;
+      expect(context.log.info).to.have.been.calledWith(
+        `[Form Opportunity] [Site Id: ${siteId}] No a11y issues found to create or update opportunity`,
       );
     });
 
@@ -220,6 +270,16 @@ describe('Forms Opportunities - Accessibility Handler', () => {
       expect(successCriteria.criteriaNumber).to.equal('1.1.1');
       expect(successCriteria.name).to.equal('Non-text Content');
       expect(successCriteria).to.have.property('understandingUrl');
+
+      // Verify SQS message was sent
+      expect(context.sqs.sendMessage).to.have.been.calledOnce;
+      const sqsMessage = context.sqs.sendMessage.getCall(0).args[1];
+      expect(sqsMessage.type).to.equal('detect:forms-a11y');
+      expect(sqsMessage.siteId).to.equal(siteId);
+      expect(sqsMessage.auditId).to.equal('test-audit-id');
+      expect(sqsMessage.deliveryType).to.equal('aem');
+      expect(sqsMessage.data.url).to.equal('https://example.com');
+      expect(sqsMessage.data.opportunityId).to.equal('test-opportunity-id');
     });
 
     it('should handle errors when processing a11y data', async () => {
@@ -287,8 +347,292 @@ describe('Forms Opportunities - Accessibility Handler', () => {
 
       await createAccessibilityOpportunity(latestAudit, context);
       expect(context.log.error).to.have.been.calledWith(
-        '[Form Opportunity] [Site Id: test-site-id] Failed to create a11y opportunity with error: Network error',
+        '[Form Opportunity] [Site Id: test-site-id] Failed to create/update a11y opportunity with error: Network error',
       );
+    });
+  });
+
+  describe('accessibility handle - mystique detected', async () => {
+    let context;
+    const siteId = 'test-site-id';
+    const auditId = 'test-audit-id';
+    const opportunityId = 'test-opportunity-id';
+
+    beforeEach(() => {
+      context = new MockContextBuilder()
+        .withSandbox(sandbox)
+        .withOverrides({
+          runtime: { name: 'aws-lambda', region: 'us-east-1' },
+          func: { package: 'spacecat-services', version: 'ci', name: 'test' },
+          site: {
+            getId: sinon.stub().returns(siteId),
+            getBaseURL: sinon.stub().returns('https://example.com'),
+            getDeliveryType: sinon.stub().returns('aem'),
+          },
+          env: {
+            QUEUE_SPACECAT_TO_MYSTIQUE: 'test-queue',
+          },
+          sqs: {
+            sendMessage: sandbox.stub().resolves(),
+          },
+          log: {
+            info: sinon.stub(),
+            error: sinon.stub(),
+          },
+          dataAccess: {
+            Opportunity: {
+              findById: sandbox.stub().resolves({
+                getId: () => opportunityId,
+                save: sandbox.stub().resolves({
+                  getId: () => opportunityId,
+                }),
+                data: {
+                  accessibility: [{
+                    form: 'https://example.com/form1',
+                    formSource: '#form1',
+                    a11yIssues: [{
+                      issue: 'Missing alt text',
+                      level: 'error',
+                      successCriterias: ['1.1.1 Non-text Content'],
+                      htmlWithIssues: ['<img src="test.jpg">'],
+                      recommendation: 'Add alt text to image',
+                    }],
+                  }],
+                },
+              }),
+              create: sandbox.stub().resolves({
+                getId: () => opportunityId,
+                setUpdatedBy: sandbox.stub(),
+                setAuditId: sandbox.stub(),
+              }),
+            },
+            Site: {
+              findById: sandbox.stub().resolves({
+                getDeliveryType: sinon.stub().returns('aem'),
+                getBaseURL: sinon.stub().returns('https://example.com'),
+              }),
+            },
+          },
+        })
+        .build();
+    });
+
+    it('should process message and send to mystique', async () => {
+      const message = {
+        auditId,
+        siteId,
+        data: {
+          opportunityId,
+          a11y: [{
+            form: 'https://example.com/form1',
+            formSource: '#form1',
+            a11yIssues: [{
+              issue: 'Missing alt text',
+              level: 'error',
+              successCriterias: ['1.1.1'],
+              htmlWithIssues: ['<img src="test.jpg">'],
+              recommendation: 'Add alt text to image',
+            }],
+          }],
+        },
+      };
+
+      await mystiqueDetectedFormAccessibilityHandler(message, context);
+
+      expect(context.sqs.sendMessage).to.have.been.calledOnce;
+      const sqsMessage = context.sqs.sendMessage.getCall(0).args[1];
+      expect(sqsMessage.type).to.equal('guidance:forms-a11y');
+      expect(sqsMessage.siteId).to.equal(siteId);
+      expect(sqsMessage.auditId).to.equal(auditId);
+      expect(sqsMessage.deliveryType).to.equal('aem');
+      expect(sqsMessage.data.opportunityId).to.equal(opportunityId);
+      expect(sqsMessage.data.a11y).to.not.exist;
+    });
+
+    it('should not send message to mystique when no opportunity is found', async () => {
+      const message = {
+        auditId,
+        siteId,
+        data: {
+          opportunityId: null,
+          a11y: [],
+        },
+      };
+
+      await mystiqueDetectedFormAccessibilityHandler(message, context);
+      expect(context.sqs.sendMessage).to.not.have.been.called;
+      expect(context.log.info).to.have.been.calledWith(
+        '[Form Opportunity] [Site Id: test-site-id] A11y opportunity not detected, skipping guidance',
+      );
+    });
+
+    it('handle error when no site is found', async () => {
+      const message = {
+        auditId,
+        siteId,
+        data: {
+          opportunityId,
+          a11y: [],
+        },
+      };
+      context.dataAccess.Site.findById.rejects(new Error('Site not found'));
+      await mystiqueDetectedFormAccessibilityHandler(message, context);
+      expect(context.log.error).to.have.been.calledWith(
+        '[Form Opportunity] [Site Id: test-site-id] Failed to process a11y opportunity from mystique: Site not found',
+      );
+    });
+
+    it('should append accessibility issue detected by mystique', async () => {
+      const message = {
+        auditId,
+        siteId,
+        data: {
+          opportunityId,
+          a11y: [{
+            form: 'https://example.com/form1',
+            formSource: '#form1',
+            a11yIssues: [{
+              issue: 'Missing alt text',
+              level: 'error',
+              successCriterias: ['1.1.1'],
+              htmlWithIssues: ['<img src="test.jpg">'],
+              recommendation: 'Add alt text to image',
+            }],
+          }],
+        },
+      };
+
+      // mock existing opportunity
+      context.dataAccess.Opportunity.findById.resolves({
+        save: sandbox.stub().resolves({
+          getId: () => opportunityId,
+        }),
+        setUpdatedBy: sandbox.stub(),
+        data: {
+          accessibility: [{
+            form: 'https://example.com/form1',
+            formSource: '#form1',
+            a11yIssues: [],
+          }],
+        },
+      });
+
+      await mystiqueDetectedFormAccessibilityHandler(message, context);
+
+      expect(context.dataAccess.Opportunity.findById).to.have.been.calledOnce;
+      const existingOpportunity = await context.dataAccess.Opportunity.findById
+        .getCall(0).returnValue;
+      expect(existingOpportunity.data.accessibility).to.have.lengthOf(1);
+      expect(existingOpportunity.data.accessibility[0].a11yIssues).to.have.lengthOf(1);
+      expect(existingOpportunity.save).to.have.been.calledOnce;
+    });
+
+    it('should append a11y issues from mystique with empty axe issues', async () => {
+      const message = {
+        auditId,
+        siteId,
+        data: {
+          opportunityId,
+          a11y: [{
+            form: 'https://example.com/form1',
+            formSource: '#form1',
+            a11yIssues: [{
+              issue: 'Missing alt text',
+              level: 'error',
+              successCriterias: ['1.1.1'],
+              htmlWithIssues: ['<img src="test.jpg">'],
+              recommendation: 'Add alt text to image',
+            }],
+          }],
+        },
+      };
+
+      // mock existing opportunity
+      context.dataAccess.Opportunity.findById.resolves({
+        save: sandbox.stub().resolves({
+          getId: () => opportunityId,
+        }),
+        setUpdatedBy: sandbox.stub(),
+        data: {
+          accessibility: [{
+            form: 'https://example.com/form2',
+            formSource: '#form2',
+            a11yIssues: [{
+              issue: 'label missing',
+              level: 'A',
+              successCriterias: ['1.1.1'],
+              htmlWithIssues: ['<label>test</label>'],
+              recommendation: 'Add label to input',
+            }],
+          }],
+        },
+      });
+
+      await mystiqueDetectedFormAccessibilityHandler(message, context);
+
+      expect(context.dataAccess.Opportunity.findById).to.have.been.calledOnce;
+      const existingOpportunity = await context.dataAccess.Opportunity.findById
+        .getCall(0).returnValue;
+      expect(existingOpportunity.data.accessibility).to.have.lengthOf(2);
+      expect(existingOpportunity.data.accessibility[0].a11yIssues).to.have.lengthOf(1);
+      expect(existingOpportunity.save).to.have.been.calledOnce;
+    });
+
+    it('should create a new opportunity when no existing opportunity is found', async () => {
+      const message = {
+        auditId,
+        siteId,
+        data: {
+          a11y: [{
+            form: 'https://example.com/form1',
+            formSource: '#form1',
+            a11yIssues: [{
+              issue: 'Missing alt text',
+              level: 'error',
+              successCriterias: ['1.1.1'],
+              htmlWithIssues: '<img src="test.jpg">',
+              recommendation: 'Add alt text to image',
+            }],
+          }],
+        },
+      };
+      await mystiqueDetectedFormAccessibilityHandler(message, context);
+      expect(context.dataAccess.Opportunity.create).to.have.been.calledOnce;
+      const createArgs = context.dataAccess.Opportunity.create.getCall(0).args[0];
+      expect(createArgs.siteId).to.equal(siteId);
+      expect(createArgs.auditId).to.equal('test-audit-id');
+      expect(createArgs.type).to.equal(FORM_OPPORTUNITY_TYPES.FORM_A11Y);
+    });
+
+    it('should handle errors when processing message', async () => {
+      const message = {
+        auditId,
+        siteId,
+        data: {
+          opportunityId,
+          a11y: [{
+            form: 'https://example.com/form1',
+            formSource: '#form1',
+            a11yIssues: [{
+              issue: 'Missing alt text',
+              level: 'error',
+              successCriterias: ['1.1.1'],
+              htmlWithIssues: '<img src="test.jpg">',
+              recommendation: 'Add alt text to image',
+            }],
+          }],
+        },
+      };
+
+      context.dataAccess.Opportunity.findById.rejects(new Error('Database error'));
+
+      try {
+        await mystiqueDetectedFormAccessibilityHandler(message, context);
+      } catch (error) {
+        expect(error.message).to.equal(
+          '[Form Opportunity] [Site Id: test-site-id] Failed to create/update a11y opportunity with error: Database error',
+        );
+      }
     });
   });
 });
