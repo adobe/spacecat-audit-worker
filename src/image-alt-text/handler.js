b45d864764a254b15070cfa06c183e91db0563a2
Sahil Silare <sahilsilareadobe@gmail.com>
2025-06-17 23:03:16 +0530

    feat: added projected traffic
    Signed-off-by: Sahil Silare <sahilsilareadobe@gmail.com>

--- src/image-alt-text/handler.js
+++ src/image-alt-text/handler.js
@@ -160,6 +160,18 @@ export async function processAltTextAuditStep(context) {
 
   await convertToOpportunity(auditUrl, auditResult, context);
 
+  // Get the opportunity to log the projected traffic values
+  const { Opportunity } = context.dataAccess;
+  const opportunities = await Opportunity.allBySiteIdAndStatus(siteId, 'NEW');
+  const altTextOppty = opportunities.find((oppty) => oppty.getType() === AUDIT_TYPE);
+  if (altTextOppty) {
+    const data = altTextOppty.getData();
+    log.info(`[${AUDIT_TYPE}] [Site Id: ${siteId}] Projected traffic values:`, {
+      projectedTrafficLost: data.projectedTrafficLost,
+      projectedTrafficValue: data.projectedTrafficValue,
+    });
+  }
+
   return {
     status: 'complete',
   };
