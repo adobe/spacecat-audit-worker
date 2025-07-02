9a74b1305b8cc72cfac7f7a55f67bbc7bbdc5f37
paraschi <paraschi@adobe.com>
2025-07-02 19:48:38 +0300

    fix: trim whitespace in the sitemap indexes and sitemap urls

--- test/audits/sitemap.test.js
+++ test/audits/sitemap.test.js
@@ -433,7 +433,7 @@ describe('Sitemap Audit', () => {
       });
     });
 
-    it('should handle URLs with whitespace in sitemap XML (like crucial.com)', async () => {
+    it('should handle URLs with whitespace in sitemap XML (ex: crucial.com)', async () => {
       const sampleSitemapWithWhitespace = '<?xml version="1.0" encoding="UTF-8"?>\n'
         + '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
         + `<url> <loc> ${url}/foo </loc></url>\n`
