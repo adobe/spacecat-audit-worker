bef2cf1c99b19f4779e039721071d6e543c83138
jjenscodee <yihsin.jen@gmail.com>
2025-07-23 13:07:27 +0200

    test

--- src/image-alt-text/guidance-missing-alt-text-handler.js
+++ src/image-alt-text/guidance-missing-alt-text-handler.js
@@ -24,6 +24,7 @@ const AUDIT_TYPE = AuditModel.AUDIT_TYPES.ALT_TEXT;
  * @returns {Array} Array of suggestions in the same format as opportunityHandler
  */
 function mapMystiqueSuggestionsToOpportunityFormat(mystiquesuggestions) {
+  console.log(`[${AUDIT_TYPE}]: Mystique suggestions for alt-text: ${mystiquesuggestions}`);
   return mystiquesuggestions.map((suggestion) => {
     const suggestionId = `${suggestion.pageUrl}/${suggestion.imageId}`;
 
