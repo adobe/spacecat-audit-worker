27c7b9c732ef6c0424f86ac5675cffe92b56042a
Akshay Magapu <amagapu@adobe.com>
2025-07-24 16:01:59 +0200

    fix: add oai searchbot

--- src/cdn-analysis/sql/fastly/unload-aggregated.sql
+++ src/cdn-analysis/sql/fastly/unload-aggregated.sql
@@ -16,7 +16,7 @@ UNLOAD (
     -- agentic and LLM-attributed traffic filter based on user-agent, referer and utm tag
     AND (
       -- match known LLM-related user-agents
-      REGEXP_LIKE(request_user_agent, '(?i)ChatGPT|GPTBot|Perplexity|Claude|Anthropic|Gemini|Copilot|Googlebot|bingbot')
+      REGEXP_LIKE(request_user_agent, '(?i)ChatGPT|GPTBot|OAI-SearchBot|Perplexity|Claude|Anthropic|Gemini|Copilot|Googlebot|bingbot')
 
       -- match known referer hostnames for LLM-attributed real-user traffic
       OR REGEXP_LIKE(COALESCE(request_referer, ''), '(?i)chatgpt\.com|openai\.com|perplexity\.ai|claude\.ai|gemini\.google\.com|copilot\.microsoft\.com')
