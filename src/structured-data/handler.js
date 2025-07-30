e57011bd1d927e04946b5c91d8859698f51afcba
noelruiz34 <noelruiz34@gmail.com>
2025-07-30 09:54:37 -0500

    add index to ensure uniqueness

--- src/structured-data/handler.js
+++ src/structured-data/handler.js
@@ -192,19 +192,7 @@ export async function opportunityAndSuggestions(auditUrl, auditData, context) {
     return { ...auditData };
   }
 
-  // Convert suggestions to errors, with duplicate removal
-  for (const issue of auditData.auditResult.issues) {
-    issue.errors = [];
-    const fix = generateErrorMarkupForIssue(issue);
-    const errorTitle = `${issue.rootType}: ${issue.issueMessage}`;
-    const errorId = errorTitle.replaceAll(/["\s]/g, '').toLowerCase();
-
-    if (!issue.errors.some((e) => e.id === errorId)) {
-      issue.errors.push({ fix, id: errorId, errorTitle });
-    }
-  }
-
-  // Convert suggestions to errors, keep duplicates and ensure unique ID using index
+  // Convert suggestions to errors
   let errorIndex = 1;
   for (const issue of auditData.auditResult.issues) {
     issue.errors = [];
