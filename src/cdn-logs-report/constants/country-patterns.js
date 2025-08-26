f155645aac910625b513b9bca57677b9e7be74dc
Akshay <akshaymagapu@users.noreply.github.com>
2025-08-26 15:03:09 +0200

    fix: reduce size of excel by grouping urls for status 2xx and 3xx (#1166)
    - Reduce size of excel by grouping urls for status 2xx and 3xx
    - make country patterns - case insensitive

--- src/cdn-logs-report/constants/country-patterns.js
+++ src/cdn-logs-report/constants/country-patterns.js
@@ -12,26 +12,29 @@
 
 export const DEFAULT_COUNTRY_PATTERNS = [
   // Matches locale with dash format: /en-us/, /fr-fr/, https://example.com/de-de/page
-  { name: 'locale_dash_full', regex: '^(?:/|(?:https?:\\/\\/|\\/\\/)?[^/]+/)?[a-z]{2}-([a-z]{2})(?:/|$)' },
+  { name: 'locale_dash_full', regex: '(?i)^(?:/|(?:https?:\\/\\/|\\/\\/)?[^/]+/)?[a-z]{2}-([a-z]{2})(?:/|$)' },
 
   // Matches locale with underscore format: /en_us/, /fr_fr/, https://example.com/de_de/page
-  { name: 'locale_underscore_full', regex: '^(?:/|(?:https?:\\/\\/|\\/\\/)?[^/]+/)?([a-z]{2})_[a-z]{2}(?:/|$)' },
+  { name: 'locale_underscore_full', regex: '(?i)^(?:/|(?:https?:\\/\\/|\\/\\/)?[^/]+/)?[a-z]{2}_([a-z]{2})(?:/|$)' },
+
+  // Matches locale files: /en_us.html, /fr_ca.jsp, etc.
+  { name: 'locale_underscore_file', regex: '(?i)^(?:/|(?:https?:\\/\\/|\\/\\/)?[^/]+/)?[a-z]{2}_([a-z]{2})\\.[a-z]+$' },
 
   // Matches global/international prefix: /global/us/, /international/fr/, https://example.com/global/de/
-  { name: 'global_prefix', regex: '^(?:/|(?:https?:\\/\\/|\\/\\/)?[^/]+/)(?:global|international)/([a-z]{2})(?:/|$)' },
+  { name: 'global_prefix', regex: '(?i)^(?:/|(?:https?:\\/\\/|\\/\\/)?[^/]+/)(?:global|international)/([a-z]{2})(?:/|$)' },
 
   // Matches countries/regions prefix: /countries/us/, /regions/fr/, https://example.com/country/de/
-  { name: 'countries_prefix', regex: '^(?:/|(?:https?:\\/\\/|\\/\\/)?[^/]+/)(?:countries?|regions?)/([a-z]{2})(?:/|$)' },
+  { name: 'countries_prefix', regex: '(?i)^(?:/|(?:https?:\\/\\/|\\/\\/)?[^/]+/)(?:countries?|regions?)/([a-z]{2})(?:/|$)' },
 
-  // Matches country/language format: /in/en/, /us/en/, https://example.com/de/en/page
-  { name: 'country_lang', regex: '^(?:/|(?:https?:\\/\\/|\\/\\/)?[^/]+/)([a-z]{2})/[a-z]{2}(?:/|$)' },
+  // Matches country/language format: /us/en/, /ca/fr/, https://example.com/de/en/page
+  { name: 'country_lang', regex: '(?i)^(?:/|(?:https?:\\/\\/|\\/\\/)?[^/]+/)([a-z]{2})/[a-z]{2}(?:/|$)' },
 
   // Matches 2-letter country codes: /us/, /fr/, /de/, https://example.com/gb/page
-  { name: 'path_2letter_full', regex: '^(?:/|(?:https?:\\/\\/|\\/\\/)?[^/]+/)?([a-z]{2})(?:/|$)' },
+  { name: 'path_2letter_full', regex: '(?i)^(?:/|(?:https?:\\/\\/|\\/\\/)?[^/]+/)?([a-z]{2})(?:/|$)' },
 
   // Matches country query parameter: ?country=us, &country=fr, ?country=usa
-  { name: 'query_country', regex: '[?&]country=([a-z]{2,3})(?:&|$)' },
+  { name: 'query_country', regex: '(?i)[?&]country=([a-z]{2,3})(?:&|$)' },
 
   // Matches locale query parameter: ?locale=en-us, &locale=fr-fr
-  { name: 'query_locale', regex: '[?&]locale=[a-z]{2}-([a-z]{2})(?:&|$)' },
+  { name: 'query_locale', regex: '(?i)[?&]locale=[a-z]{2}-([a-z]{2})(?:&|$)' },
 ];
