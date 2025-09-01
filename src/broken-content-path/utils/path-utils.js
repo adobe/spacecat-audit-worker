4cce0bcfc89cb24fb2e60564f671ecc152b71a23
Bjoern Holtvogt <bholtvogt@adobe.com>
2025-09-01 11:47:16 +0200

    Add PathUtils class for content path manipulation
    This commit introduces the PathUtils class, which provides utility methods for managing content paths. The class includes methods to remove locale segments from paths and to retrieve the parent path of a given content path, enhancing the overall path management functionality.

---
+++ src/broken-content-path/utils/path-utils.js
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2025 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+import { Locale } from '../domain/language/locale.js';
+
+export class PathUtils {
+  static removeLocaleFromPath(path) {
+    if (!path || !path.startsWith('/content/dam/')) {
+      return path;
+    }
+
+    let removedPath = path;
+    // Remove trailing slash from the path
+    const endsWithSlash = removedPath.endsWith('/');
+    if (endsWithSlash) {
+      removedPath = removedPath.slice(0, -1);
+    }
+
+    const segments = removedPath.split('/');
+    const result = [];
+    let hasLocale = false;
+
+    for (const segment of segments) {
+      const isLocale = segment.match(Locale.TWO_LETTER_PATTERN)
+                      || segment.match(Locale.FIVE_LETTER_PATTERN);
+      if (isLocale) {
+        hasLocale = true;
+      } else {
+        result.push(segment);
+      }
+    }
+
+    if (!hasLocale) {
+      return endsWithSlash ? path : removedPath;
+    }
+
+    return result.join('/');
+  }
+
+  static getParentPath(path) {
+    if (!path || !path.startsWith('/content/dam/')) {
+      return null;
+    }
+
+    let removedPath = path;
+
+    // Remove trailing slash from the path
+    if (path.endsWith('/')) {
+      removedPath = removedPath.slice(0, -1);
+    }
+
+    return removedPath.substring(0, removedPath.lastIndexOf('/'));
+  }
+}
