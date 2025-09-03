8e0baf7f763dffc1206b474fd20cb38fb9f8e778
Rahul Kumar Singh <rahul@Rahuls-MacBook-Pro-5.local>
2025-09-03 20:34:39 +0530

    fix: incorporating forms quality agent in  forms a11y oppties

--- src/forms-opportunities/utils.js
+++ src/forms-opportunities/utils.js
@@ -509,7 +509,7 @@ export async function sendMessageToFormsQualityAgent(context, opportunity, forms
     const mystiqueFormsQualityAgentMessage = {
       type: 'detect:form-details',
       siteId: opportunityData.siteId,
-      auditId: opportunityData.id,
+      auditId: opportunityData.auditId,
       deliveryType: site.getDeliveryType(),
       time: new Date().toISOString(),
       data,
