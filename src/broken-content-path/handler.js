95d3c2164735079110b899ed6422f06bd06c1fc3
Bjoern Holtvogt <bholtvogt@adobe.com>
2025-09-15 11:42:23 +0200

    Add a newline in handler.js

--- src/broken-content-path/handler.js
+++ src/broken-content-path/handler.js
@@ -9,6 +9,7 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
+
 import { Audit } from '@adobe/spacecat-shared-data-access';
 import { AuditBuilder } from '../common/audit-builder.js';
 import { CollectorFactory } from './collectors/collector-factory.js';
