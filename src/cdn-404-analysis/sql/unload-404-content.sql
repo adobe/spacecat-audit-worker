6439368bbc9099f22dc1ba0f41ad1d1a7bdc918b
Bjoern Holtvogt <bholtvogt@adobe.com>
2025-09-15 17:26:02 +0200

    Use rawTable as dynamic table name

--- src/cdn-404-analysis/sql/unload-404-content.sql
+++ src/cdn-404-analysis/sql/unload-404-content.sql
@@ -3,7 +3,7 @@ UNLOAD (
     url AS url,
     COALESCE(REGEXP_EXTRACT(url, '/content/dam/([^/]+)', 1), 'unknown') AS tenant,
     count(*) AS count
-  FROM {{database}}.{{tableName}}
+  FROM {{database}}.{{rawTable}}
   WHERE year  = '{{year}}'
     AND month = '{{month}}'
     AND day   = '{{day}}'
