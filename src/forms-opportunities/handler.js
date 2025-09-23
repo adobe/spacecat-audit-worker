098fb9b708923f5600c0a6a5a7e4c73f189871cf
Rahul Kumar Singh <rahul@Rahuls-MacBook-Pro-5.local>
2025-09-23 14:31:57 +0530

    fix: disable caching for forms content scraper

--- src/forms-opportunities/handler.js
+++ src/forms-opportunities/handler.js
@@ -142,7 +142,7 @@ export async function runAuditAndSendUrlsForScrapingStep(context) {
 
   const result = {
     processingType: 'form',
-    allowCache: true,
+    allowCache: false,
     jobId: site.getId(),
     urls: urlsData,
     siteId: site.getId(),
