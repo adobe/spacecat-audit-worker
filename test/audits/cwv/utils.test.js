d204ad5e7dc1b539ce7bc9053af30c6e63b6ff87
naydav <vnayda@adobe.com>
2025-10-08 16:37:54 -0500

    fix: complete CWV SQS integration with proper test compatibility

--- test/audits/cwv/utils.test.js
+++ test/audits/cwv/utils.test.js
@@ -14,6 +14,7 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
 import { sendMessageToMystiqueForGuidance } from '../../../src/cwv/utils.js';
+import { Audit } from '@adobe/spacecat-shared-data-access';
 
 describe('sendMessageToMystiqueForGuidance', () => {
   let context;
