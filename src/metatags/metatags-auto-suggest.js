86e230ae4651dc2e962aa48092cb10656fa13db9
Bohdan Korablov <korablov@adobe.com>
2025-11-04 10:12:33 -0600

    redeploy

--- src/metatags/metatags-auto-suggest.js
+++ src/metatags/metatags-auto-suggest.js
@@ -55,7 +55,7 @@ export default async function metatagsAutoSuggest(allTags, context, site, option
   let responseWithSuggestions;
   try {
     log.info(`GENVAR_HOST ${site.getId()} ${context.env.GENVAR_HOST}`);
-    log.info(`===requestBody ${site.getId()} ${JSON.stringify(requestBody)}`);
+    log.info(`====requestBody ${site.getId()} ${JSON.stringify(requestBody)}`);
     const genvarClient = GenvarClient.createFrom(context);
     responseWithSuggestions = await genvarClient.generateSuggestions(
       JSON.stringify(requestBody),
