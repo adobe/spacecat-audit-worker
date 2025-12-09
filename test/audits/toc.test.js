f170e8c536e87086aa3b4f551de349335ed0d7dd
anagarwa <anagarwa@adobe.com>
2025-12-09 14:04:54 +0530

    fix: content oppty titles

--- test/audits/toc.test.js
+++ test/audits/toc.test.js
@@ -1300,7 +1300,7 @@ describe('TOC (Table of Contents) Audit', () => {
       expect(opportunityData).to.be.an('object');
       expect(opportunityData).to.have.property('runbook', '');
       expect(opportunityData).to.have.property('origin', 'AUTOMATION');
-      expect(opportunityData).to.have.property('title', 'Table of contents issues affecting accessibility and SEO');
+      expect(opportunityData).to.have.property('title', 'Add Table of Content');
       expect(opportunityData).to.have.property('description');
       expect(opportunityData.description).to.include('table of contents');
       expect(opportunityData.description).to.include('AI-powered suggestions');
