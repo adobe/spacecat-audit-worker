47908ec6f62959861f31ffd1e19dd387752fdb9c
ctinp <115073531+ctinp@users.noreply.github.com>
2025-12-12 16:16:11 +0100

    feat: LLMO-1925 audits support cdn logs coming from 'other' sources (#1792)
    Please ensure your pull request adheres to the following guidelines:
    - [ ] make sure to link the related issues in this description
    - [ ] when merging / squashing, make sure the fixed issue references are
    visible in the commits, for easy compilation of release notes
    - [ ] If data sources for any opportunity has been updated/added, please
    update the
    [wiki](https://wiki.corp.adobe.com/display/AEMSites/Data+Sources+for+Opportunities)
    for same opportunity.
    
    ## Related Issues
    
    
    Thanks for contributing!

--- test/utils/cdn-utils.test.js
+++ test/utils/cdn-utils.test.js
@@ -16,6 +16,7 @@ import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import {
   CDN_TYPES,
+  SERVICE_PROVIDER_TYPES,
   extractCustomerDomain,
   resolveCdnBucketName,
   buildCdnPaths,
@@ -24,6 +25,7 @@ import {
   isStandardAdobeCdnBucket,
   shouldRecreateRawTable,
   buildSiteFilters,
+  mapServiceToCdnProvider,
 } from '../../src/utils/cdn-utils.js';
 
 use(sinonChai);
@@ -47,7 +49,16 @@ describe('CDN Utils', () => {
         CLOUDFLARE: 'cloudflare',
         CLOUDFRONT: 'cloudfront',
         FRONTDOOR: 'frontdoor',
+        OTHER: 'other',
       });
+    });
+  });
+
+  describe('mapServiceToCdnProvider', () => {
+    it('maps byocdn-other to OTHER cdn type', () => {
+      expect(
+        mapServiceToCdnProvider(SERVICE_PROVIDER_TYPES.BYOCDN_OTHER),
+      ).to.equal(CDN_TYPES.OTHER);
     });
   });
 
@@ -187,6 +198,8 @@ describe('CDN Utils', () => {
 
   describe('getBucketInfo', () => {
     let s3Client;
+    const bucketName = 'cdn-logs-adobe-prod';
+    const pathId = 'ims-org-123';
 
     beforeEach(() => {
       s3Client = { send: sandbox.stub() };
@@ -208,6 +221,17 @@ describe('CDN Utils', () => {
 
       expect(result.isLegacy).to.be.true;
       expect(result.providers).to.deep.equal([]);
+    });
+
+    it('returns modern bucket info when byocdn-other prefix exists', async () => {
+      s3Client.send.resolves({
+        CommonPrefixes: [{ Prefix: `${pathId}/raw/byocdn-other/` }],
+      });
+
+      const result = await getBucketInfo(s3Client, bucketName, pathId);
+
+      expect(result.isLegacy).to.be.false;
+      expect(result.providers).to.deep.equal(['byocdn-other']);
     });
   });
 
