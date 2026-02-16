330cfd23f64d96a1439ce8ee0a259c3de2677ce7
Alberto Di Cagno <adicagno@adobe.com>
2026-02-16 18:26:53 +0100

    chore: cleanup

--- src/geo-brand-presence/detect-geo-brand-presence-handler.js
+++ src/geo-brand-presence/detect-geo-brand-presence-handler.js
@@ -129,11 +129,11 @@ export default async function handler(message, context) {
   const sheetSize = sheet.byteLength;
   log.info(`%s: Sheet fetched successfully for auditId: ${auditId}, siteId: ${siteId} (${sheetSize} bytes in ${fetchDuration}ms)`, AUDIT_NAME);
 
-  const xlsxName = extractXlsxName(presignedURL);
   // upload to sharepoint & publish via hlx admin api
   log.info(`%s: Creating SharePoint client for auditId: ${auditId}, siteId: ${siteId}`, AUDIT_NAME);
   const sharepointClient = await createLLMOSharepointClient(context);
 
+  const xlsxName = extractXlsxName(presignedURL);
   log.info(`%s: Starting SharePoint upload for auditId: ${auditId}, siteId: ${siteId}, file: ${xlsxName}, locations: ${outputLocations.length}`, AUDIT_NAME);
 
   const uploadStartTime = Date.now();
