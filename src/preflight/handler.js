154e380adbb07490153de92096e48cb321dbdef0
Radhika Gupta <66867130+radhikagpt1208@users.noreply.github.com>
2026-03-11 21:01:24 +0530

    chore: trigger deployment

--- src/preflight/handler.js
+++ src/preflight/handler.js
@@ -8,7 +8,7 @@
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
- */
+ */ 
 
 import { isValidUrl, stripTrailingSlash } from '@adobe/spacecat-shared-utils';
 import { Audit, AsyncJob } from '@adobe/spacecat-shared-data-access';
