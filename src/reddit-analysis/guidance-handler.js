bd01a33c8a5801123d49c5e7556e8d9bde595f92
TB ADBE <247622345+tb-adbe@users.noreply.github.com>
2026-03-12 17:22:18 +0200

    Refactor/reddit anlaysis generic (#2121)
    Please ensure your pull request adheres to the following guidelines:
    - [ ] make sure to link the related issues in this description
    - [ ] when merging / squashing, make sure the fixed issue references are
    visible in the commits, for easy compilation of release notes
    - [ ] If data sources for any opportunity has been updated/added, please
    update the
    [wiki](https://wiki.corp.adobe.com/display/AEMSites/Data+Sources+for+Opportunities)
    for same opportunity.
    
    ## Related Issues
    
    
    Thanks for contributing!
    
    ---------
    
    Co-authored-by: Tanase Butcaru <tbutcaru@adobe.com>

--- src/reddit-analysis/guidance-handler.js
+++ src/reddit-analysis/guidance-handler.js
@@ -14,14 +14,13 @@ import {
   badRequest, notFound, ok, noContent,
 } from '@adobe/spacecat-shared-http-utils';
 import { tracingFetch as fetch } from '@adobe/spacecat-shared-utils';
-// import { Audit } from '@adobe/spacecat-shared-data-access';
+import { Audit } from '@adobe/spacecat-shared-data-access';
 
 import { syncSuggestions } from '../utils/data-access.js';
 import { createOpportunityData } from './opportunity-data-mapper.js';
 import { convertToOpportunity } from '../common/opportunity.js';
 
-// TODO: switch back to `Audit.AUDIT_TYPES.REDDIT_ANALYSIS` once native ui is ready
-const AUDIT_TYPE = 'reddit-analysis-generic';
+const AUDIT_TYPE = Audit.AUDIT_TYPES.REDDIT_ANALYSIS;
 
 /**
  * Gets rank based on priority
